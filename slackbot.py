93c6c8642f4213f7a29f55b84c07063c7d4a7026
rinle <rinle.it@gmail.com>
2018-08-01 22:36:02 +0700

    FIX: Bug fixes

--- slackbot.py
+++ slackbot.py
@@ -4,6 +4,7 @@ import os
 import time
 import re
 import argparse
+from random import randint
 from datetime import datetime, timedelta
 from slackclient import SlackClient
 from logger import setup_logger
@@ -18,21 +19,19 @@ botlog = logging.getLogger("slackbot")
 
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
-NOTIFY = None
+NOTIFY = []
 
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
 LIVE_BOT = 'ping'
-ALT_MONITOR_START = 'monitor start'
-ALT_MONITOR_STOP = 'monitor stop'
-MONITOR_TODAY = 'monitor t0'
-MONITOR_TOMOR = 'monitor t1'
-MONITOR_TOMOR2 = 'monitor t2'
+ALT_MONITOR_SUB = 'sub'
+ALT_MONITOR_UNSUB = 'unsub'
+MONITOR_TODAY = 't0'
+MONITOR_TOMOR = 't1'
+MONITOR_TOMOR2 = 't2'
 SENT2CHANNEL = 'send'
 SETCHANNEL = 'setchannel'
-SETWORKTIME = 'setworktime'
-SETLUNCHTIME = 'setlunchtime'
 
 LUNCH_TIME = '11:45:00'
 WORK_TIME = '12:45:00'
@@ -52,7 +51,6 @@ FRIDAY = 'Fri'
 SATDAY = 'Sat'
 SUNDAY = 'Sun'
 HOURNOTIFY = 17
-
 
 def parse_bot_commands(slack_events):
     global  starterbot_id
@@ -77,11 +75,21 @@ def parse_direct_mention(message_text):
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+def append_channel(channel):
+    global NOTIFY
+    if channel not in NOTIFY:
+        NOTIFY.append(channel)
+        botlog.debug("Appen_channel >> %s" % (channel))
+
+def remove_channel(channel):
+    global NOTIFY
+    if channel in NOTIFY:
+        NOTIFY.remove(channel)
+        botlog.debug("Remove_channel >> %s" % (channel))
+
 def handle_command(command, channel):
     global NOTIFY
     global TODAY
-    global WORK_TIME
-    global LUNCH_TIME
 
     botlog.debug("command: %s, channel: %s" % (command, channel))
     """
@@ -95,47 +103,41 @@ def handle_command(command, channel):
     # This is where you start to implement more commands!
     if command.startswith(EXAMPLE_COMMAND):
         response = """Chào bạn, mình là altBot thuộc cty ALTISSS,
-        Mình có thể làm những nhiệm vụ sau:
-        + Start Service thông báo lịch Monitor: monitor start
-        + Stop Service thông báo lịch Monitor: monitor stop
-        + Kiểm tra lịch :
-        \t- hôm nay : monitor t0
-        \t- ngày mai: monitor t1
-        \t- ngày mốt: monitor t2
-        CÁM ƠN BẠN ĐÃ SỬ DỤNG altBot nhé ^^
-        """
+Mình là botchat hỗ trợ thông báo lịch monitor:
+    + Để nhận thông báo gõ: @altBot sub
+    + Bỏ nhận thông báo gõ: @altBot unsub
+    + Kiểm tra lịch :
+    \t- Hôm nay : @altBot t0
+    \t- Ngày mai: @altBot t1
+    \t- Ngày mốt: @altBot t2
+CÁM ƠN BẠN ĐÃ SỬ DỤNG altBot nhé :joy:"""
     elif command.startswith(SENT2CHANNEL):
         target = command.split("|")[1]
         text = command.split("|")[2]
         channel = target
         response = text
     elif command.startswith(SETCHANNEL):
-        newchannel = command.split("|")[1]
-        NOTIFY = newchannel
-        response = "Đã chuyển channel notify sang %s" % (NOTIFY)
-    elif command.startswith(SETLUNCHTIME):
-        newtime = command.split(" ")[1]
-        LUNCH_TIME = newtime
-        response = "Đã chuyển thời gian nghỉ trưa sang %s" % (newtime)
-    elif command.startswith(SETWORKTIME):
-        newtime = command.split(" ")[1]
-        WORK_TIME = newtime
-        response = "Đã chuyển thời gian làm việc lại sang %s" % (newtime)
+        try:
+            newchannel = command.split(" ")[1]
+            append_channel(newchannel)
+            response = "Channel [%s] vừa được kích hoạt nhận thông báo" % (newchannel)
+        except:
+            botlog.debug("Error command (%s)" % (command))
     elif command.startswith(LIVE_BOT):
         response = "Sure... I'm heartbeat with you"
-    elif command.startswith(ALT_MONITOR_START):
-        NOTIFY = channel
-        response = "Service ALT MONITOR đã được start!\n Chúc bạn ngày mới vui vẻ!"
-    elif command.startswith(ALT_MONITOR_STOP):
-        NOTIFY = None
-        response = "Sure... Services ALT MONITOR đã stop !!!"
+    elif command.startswith(ALT_MONITOR_SUB):
+        append_channel(channel)
+        response = "Đã kích hoạt để nhận thông báo"
+    elif command.startswith(ALT_MONITOR_UNSUB):
+        remove_channel(channel)
+        response = "Đã hủy kích hoạt nhận thông báo"
     elif command.startswith(MONITOR_TODAY):
         today = datetime.now()
         NameDay = str(today.now().strftime('%a'))
         if TODAY != None:
             response = "Lịch monitor hôm nay (%s) là (%s)" % (str(today.strftime("%a, %d %b %Y")), TEAM[str(TODAY)])
         else:
-            response = "Hình như bạn chưa start service !!! Thử gõ %s" % (ALT_MONITOR_START)
+            response = "Hình như bạn chưa subscribe để nhận thông báo lịch monitor !!! Thử gõ @altBot %s" % (ALT_MONITOR_SUB)
             if NameDay in WEEKENDAY2:
                 response = "Chúc bạn cuối tuần vui vẻ :kissing_heart: !!!"
     elif command.startswith(MONITOR_TOMOR):
@@ -145,7 +147,7 @@ def handle_command(command, channel):
         tomor = today + timedelta(1)
 
         if NameDay in WEEKENDAY:
-            response = "Ngày mai không monitor nhé cậu ^^, ở nhà đi chơi với người yêu đi nhé :joy:"
+            response = "Ngày mai không monitor nhé cậu ^^, ở nhà dành thời gian cho gia đình/bạn bè nè"
         else:
             if int(Dhour) >= int(HOURNOTIFY) or NameDay == SUNDAY:
                 response = "Lịch monitor ngày mai (%s) là (%s)" % (str(tomor.strftime("%a, %d %b %Y")), TEAM[str(LICHTRUC[0])])
@@ -159,7 +161,7 @@ def handle_command(command, channel):
         tomor2 = today + timedelta(2)
 
         if NameDay == FRIDAY:
-            response = "Ngày mốt không monitor nhé cậu ^^, ở nhà ngủ cho thoải thích nhé :*"
+            response = "Ngày mốt không monitor nhé cậu ^^, ở nhà ngủ cho thoải thích nhé :kissing_heart:"
         elif NameDay == SATDAY:
             response = "Lịch monitor ngày mốt (%s) là (%s)" % (str(tomor2.strftime("%a, %d %b %Y")), TEAM[str(LICHTRUC[0])])
         elif NameDay == SUNDAY:
@@ -180,15 +182,16 @@ def handle_command(command, channel):
     )
 
 def sendtoSlack(res, channel):
-    # Sends the response back to the channel
-    botlog.debug("channel: %s, text: %s" % (channel, res))
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=res
-    )
+    for child in channel:
+        # Sends the response back to the channel
+        botlog.debug("channel: %s, text: %s" % (channel, res))
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=child,
+            text=res
+        )
 
-def main(startday):
+def main():
     global starterbot_id
     global LICHTRUC
     global TODAY
@@ -201,12 +204,11 @@ def main(startday):
             command, channel = parse_bot_commands(slack_client.rtm_read())
             if command:
                 handle_command(command, channel)
-            if NOTIFY != None:
+            if NOTIFY != []:
                 today = datetime.now()
                 tomorrow = today + timedelta(1)
-                tomorrow2 = today + timedelta(3)
                 Dhour = str(today.strftime('%H'))
-                FullTime = str(today.strftime('%H:%M:%S'))
+                # FullTime = str(today.strftime('%H:%M:%S'))
                 NameDay = str(today.now().strftime('%a'))
                 if NameDay in DAYWORK:
                     res = None
@@ -215,6 +217,7 @@ def main(startday):
                             TODAY = LICHTRUC.pop(0)
                             LICHTRUC.append(TODAY)
                             if NameDay == FRIDAY:
+                                tomorrow2 = today + timedelta(3)
                                 res = "Lịch Monitor ngày thứ hai (%s) là (%s) ^^\nChúc mọi người cuối tuần vui nhé nhé :kissing_heart:" % (str(datetime.strftime(tomorrow2,'%Y-%m-%d')),TEAM[str(LICHTRUC[0])])
                                 sendtoSlack(res, NOTIFY)
                                 HAVESENT = True
@@ -223,26 +226,14 @@ def main(startday):
                                 sendtoSlack(res, NOTIFY)
                                 HAVESENT = True
                         else:
-                            TODAY = LICHTRUC[0] 
-                            if FullTime == startday:
-                                res = "Nhắc lại kẻo quên, Hôm nay %s %s monitor đó, Hai cậu nhớ dậy sớm đi nhé :joy:" % (str(datetime.strftime(today,'%Y-%m-%d')),TEAM[str(TODAY)])
-                                sendtoSlack(res, NOTIFY)
+                            TODAY = LICHTRUC[0]
                             HAVESENT == False
-                            
-                    if FullTime == LUNCH_TIME:
-                        res = "Đến giờ ăn trưa rồi, mọi người nghỉ trưa vui vẻ nhé :kissing_heart:"
-                        sendtoSlack(res, NOTIFY)
-                        time.sleep(3)
-                    if FullTime == WORK_TIME:
-                        res = "Hết giờ nghỉ trưa rồi đó, dậy làm việc lại thôi nào !!! :kissing_heart:"
-                        sendtoSlack(res, NOTIFY)
-                        time.sleep(3)
                 else:
                     TODAY = None
                     if HAVESENT == False:
                         if NameDay == SUNDAY:
                             if int(Dhour) >= 19:
-                                res = "Tạm biệt cuối tuần, mình trở lại nhiệm vụ nè\nLịch Monitor ngày mai (%s) là (%s)\n, Hai cậu nhớ đi nhé :joy:" % (str(datetime.strftime(tomorrow,'%Y-%m-%d')),TEAM[str(LICHTRUC[0])])
+                                res = "Tạm biệt cuối tuần, mình trở lại nhiệm vụ nhé\nLịch Monitor ngày mai (%s) là (%s) :joy:" % (str(datetime.strftime(tomorrow,'%Y-%m-%d')),TEAM[str(LICHTRUC[0])])
                                 sendtoSlack(res, NOTIFY)
                                 HAVESENT = True
                             else:
@@ -253,12 +244,11 @@ def main(startday):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Slack Client')
-    parser.add_argument('startday', type=str, help='Time')
-    parser.add_argument('endday', type=str, help='Time')
+    parser.add_argument('endday', type=str, help='endday')
     args = parser.parse_args()
     HOURNOTIFY = str(args.endday)
     try:
-        main(args.startday)
+        main()
     except (KeyboardInterrupt, SystemExit) as e:
         res = "Service altBot buộc Stop, User: (%s)!!!\n" % (str(os.environ.get('USER')))
         sendtoSlack(res, NOTIFY)
